fb58a55c3b1f36c4e7c67e10c005b9cd7c8272dc
Horlakz <officialhorlakz@gmail.com>
2022-10-19 15:31:24 +0100

    updates jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
 /** @type {import('jest').Config} */
 const config = {
   setupFilesAfterEnv: ["<rootDir>/setup-jest.js"],
+  testEnvironment: "node",
+  testMatch: ["**/**/*.test.ts"],
+  verbose: true,
+  forceExit: true,
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true,
 };
 
 module.exports = config;
